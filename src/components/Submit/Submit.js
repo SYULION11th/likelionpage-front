482d00c4cc1ff2cfe6b15285e81039aee8654808
WOOJINCHO98 <steamedbunwith@gmail.com>
2023-02-04 23:53:58 +0900

    submit page write

--- src/components/Submit/Submit.js
+++ src/components/Submit/Submit.js
@@ -1,16 +1,45 @@
-import React from "react";
-import { Container,Row,Col } from "react-bootstrap";
+import React, {useState} from "react";
+import {Container, Row, Col} from "react-bootstrap";
 function Submit() {
-  return (
-    <Container fluid>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-        <Col md={12}>
-          <div className="mainBack"></div>
-          <h1 className="Title">지원</h1>
-        </Col>
-      </Row>
-    </Container>
-  );
+
+    const [textValue, setTextValue] = useState("");
+    const handleSetValue = (e) => {
+        setTextValue(e.target.value);
+    };
+
+    return (
+        <Container fluid="fluid" className="p-0">
+            <Row
+                style={{
+                    justifyContent: "center",
+                }}>
+                <Col
+                    md={12}
+                    style={{
+                        backgroundColor: "#0A3479",
+                        minHeight: "90vh"
+                    }}>
+                    <div className="SubmitTitleBox">
+                        <h3 className="SubmitTitle">멋쟁이사자처럼 삼육대학교 11기 아기사자 지원</h3>
+                        <div className="SubmitLine">
+
+                        </div>
+                        <p className="SubmitSubTitle">
+                            POSSIBILITY TO REALITY. 멋사와 함께 가능성을 현실로.
+                        </p>
+                    </div>
+
+                    <textarea
+                        className="submit-textarea"
+                        placeholder="여기에 입력하세요"
+                        value={textValue}
+                        onChange={(e) => handleSetValue(e)}></textarea>
+                    <p className="paragraph">{textValue}</p>
+
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default Submit;
