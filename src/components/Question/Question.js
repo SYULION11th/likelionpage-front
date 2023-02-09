79ee531718df83d005c52204b215a7981d49566b
WOOJINCHO98 <steamedbunwith@gmail.com>
2023-02-09 17:33:55 +0900

    Service Preparing

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -2,11 +2,27 @@ import React from "react";
 import { Container,Row,Col } from "react-bootstrap";
 function Question() {
   return (
-    <Container fluid>
+    <Container
+    fluid="fluid"
+    className="p-0"
+    style={{
+        backgroundColor: "#0A3479",
+    }}>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-        <Col md={12}>
+        <Col md={12}
+        style={{ height: "600px"}}>
           <div className="mainBack"></div>
-          <h1 className="Title">Q&A</h1>
+          <h1 className="Title"
+          style={{
+            color: "white",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            fontSize: "50px",
+            fontWeight: "bold",
+          
+          }}>서비스 준비중 입니다.</h1>
         </Col>
       </Row>
     </Container>
