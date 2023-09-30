5f96e61bb5c46b2dc5c85a5116e120f2510f51b6
WOOJINCHO98 <steamedbunwith@gmail.com>
2023-09-30 19:48:50 +0900

    edit : 축제 홍보용 로직 수정

--- src/components/Main/SubmitButton.js
+++ src/components/Main/SubmitButton.js
@@ -1,22 +1,34 @@
-import {React} from "react";
-import {Link} from "react-router-dom";
-
+import { React } from "react";
+import { Link } from "react-router-dom";
 
 function SubmitButton(props) {
-    const scrollToTop = () => {
-      window.scroll({
-        top: 0,
-        behavior: 'smooth'
-      })
-  
-    }
+  const scrollToTop = () => {
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
-    return (
-        <Link className="SubmitButton" as={Link} to="/Submit" onClick={scrollToTop}>
-          <br></br>
-          11기 아기사자 지원하기
-        </Link>
-    );
+  //   return (
+  //     <Link className="SubmitButton" as={Link} to="/Submit" onClick={scrollToTop}>
+  //       <br></br>
+  //       11기 아기사자 지원하기
+  //     </Link>
+  // );
+  // 축제 홍보용 수정
+  return (
+    <Link
+      className="SubmitButton"
+      as={Link}
+      to="/"
+      onClick={() => {
+        alert("11기 지원이 마감되었습니다. 12기로 만나요!");
+      }}
+    >
+      <br></br>
+      11기 아기사자 지원하기
+    </Link>
+  );
 }
 
 export default SubmitButton;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,74 +1,82 @@
-import {React, useState} from "react";
-import Navbar from 'react-bootstrap/Navbar';
+import { React, useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
-import Nav from 'react-bootstrap/Nav';
-import {Link} from 'react-router-dom';
-import logo from '../Assets/logo.png';
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
+import logo from "../Assets/logo.png";
 
 function NavBar() {
-    const [expand, updateExpanded] = useState(false);
-    const [navColour, updateNavbar] = useState(false);
+  const [expand, updateExpanded] = useState(false);
+  const [navColour, updateNavbar] = useState(false);
 
-    function scrollHandler() {
-        if (window.scrollY >= 20) {
-            updateNavbar(true);
-        } else {
-            updateNavbar(false);
-        }
+  function scrollHandler() {
+    if (window.scrollY >= 20) {
+      updateNavbar(true);
+    } else {
+      updateNavbar(false);
     }
+  }
 
-    window.addEventListener("scroll", scrollHandler);
+  window.addEventListener("scroll", scrollHandler);
 
+  return (
+    <Navbar
+      bg="light"
+      expanded={expand}
+      fixed="top"
+      expand="md"
+      className={navColour ? "sticky" : "navbar"}
+    >
+      <Container>
+        <Navbar.Brand href="/likelionpage-front/" className="d-flex">
+          <img src={logo} className="img-fluid navLogo" alt="brand" />
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={() => {
+            updateExpanded(expand ? false : "expanded");
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto" defaultActiveKey="#home">
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/Question"
+                onClick={() => updateExpanded(false)}
+              >
+                Q&A
+              </Nav.Link>
+            </Nav.Item>
 
-    return (
-        <Navbar
-            bg="light"
-            expanded={expand}
-            fixed="top"
-            expand="md"
-            className={navColour
-                ? "sticky"
-                : "navbar"}>
-            <Container>
+            <Nav.Item>
+              {/* <Nav.Link
+                as={Link}
+                to="/Submit"
+                onClick={() => updateExpanded(false)}
+              >
+                지원하기
+              </Nav.Link> */}
 
-                <Navbar.Brand href="/likelionpage-front/" className="d-flex">
-                    <img src={logo} className="img-fluid navLogo" alt="brand"/>
-                </Navbar.Brand>
-                <Navbar.Toggle
-                    aria-controls="responsive-navbar-nav"
-                    onClick={() => {
-                        updateExpanded(
-                            expand
-                                ? false
-                                : "expanded"
-                        );
-                    }}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </Navbar.Toggle>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ms-auto" defaultActiveKey="#home">
-
-                        <Nav.Item>
-                            <Nav.Link as={Link} to="/Question" onClick={() => updateExpanded(false)}>
-
-                                Q&A
-                            </Nav.Link>
-                        </Nav.Item>
-
-                        <Nav.Item>
-                            <Nav.Link as={Link} to="/Submit" onClick={() => updateExpanded(false)}>
-
-                                지원하기
-                            </Nav.Link>
-                        </Nav.Item>
-
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+              <Nav.Link
+                as={Link}
+                to="/"
+                onClick={() =>
+                  alert("11기 지원이 마감되었습니다. 12기로 만나요!")
+                }
+              >
+                지원하기
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavBar;
